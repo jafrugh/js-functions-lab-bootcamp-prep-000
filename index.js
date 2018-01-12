2cb831c2a75578fb1589d355a200461a2fe03855
Adam Darling <adamad@hotmail.com>
2018-01-12 04:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 
-
-
-function happyHolidaysTo(you) {
+function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
+}
+
+function happyHolidayTo() {
+  
 }
