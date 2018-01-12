167f29a35ec6fcb7df539d7d4a88249bae5613bd
Adam Darling <adamad@hotmail.com>
2018-01-12 04:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
-function happyHolidayTo() {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown() {
-  return 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
 }
